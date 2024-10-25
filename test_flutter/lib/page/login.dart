80267f3b2c02e23cef98cd86cfb3114d326318e0
RafiqulRazu <158472447+RafiqulRazu@users.noreply.github.com>
2024-10-25 21:14:11 +0600

    login
    24.10.24

---
+++ test_flutter/lib/page/login.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Login extends StatelessWidget {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: email,
+              decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email)),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            TextField(
+              controller: password,
+              decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.password)),
+              obscureText: true,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                String em = email.text;
+                String pass = password.text;
+                print('Email: $em, , Password: $pass');
+              },
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  fontFamily: GoogleFonts.lato().fontFamily,
+                ),
+              ),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueAccent,
+                foregroundColor: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
